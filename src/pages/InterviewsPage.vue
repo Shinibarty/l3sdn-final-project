e27afd42e2693959286eef7bb078ec65f3308081
alban stievenard <albanstievenard59@gmail.com>
2024-04-15 22:43:47 +0200

    premiere version mes entretiens, reste à scinder en à manager et managé

--- src/pages/InterviewsPage.vue
+++ src/pages/InterviewsPage.vue
@@ -2,15 +2,27 @@
   <div class="interviews-page">
     <q-page class="q-pa-md">
       <h2 class="text-h5">Mes entretiens</h2>
-      <div v-if="entretiensUtilisateur.length === 0" class="text-center q-mt-lg">
+      <!-- Pas d'entretiens -->
+      <div v-if="entretiensEnAttente.length === 0 && entretiensCompletes.length === 0" class="text-center q-mt-lg">
         Aucun entretien trouvé.
       </div>
-      <div v-else>
-        <q-card v-for="entretien in entretiensUtilisateur" :key="entretien.id" class="q-mb-md">
+      <div>
+        <!-- Entretiens en attente -->
+        <h3 v-if="entretiensEnAttente.length > 0" class="text-h6">Entretiens en attente</h3>
+        <q-card v-for="entretien in entretiensEnAttente" :key="entretien.id" class="q-mb-md bg-blue-grey-3">
           <q-card-section>
             <div class="row">
               <div class="col">Date : {{ entretien.date }}</div>
-              <div class="col">Statut : {{ entretien.status }}</div>
+            </div>
+            <div class="q-mt-sm">Notes : {{ entretien.notes }}</div>
+          </q-card-section>
+        </q-card>
+        <!-- Entretiens complétés-->
+        <h3 v-if="entretiensCompletes.length > 0" class="text-h6 q-mt-md">Entretiens complétés</h3>
+        <q-card v-for="entretien in entretiensCompletes" :key="entretien.id" class="q-mb-md bg-light-green-5">
+          <q-card-section>
+            <div class="row">
+              <div class="col">Date : {{ entretien.date }}</div>
             </div>
             <div class="q-mt-sm">Notes : {{ entretien.notes }}</div>
           </q-card-section>
@@ -22,14 +34,25 @@
 
 <script setup>
 import { useAuthStore } from 'src/stores/auth'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import entretiensData from '../../public/entretiens.json'
 
 const authStore = useAuthStore()
 const userId = authStore.user.id
 
-// Filtre
 const entretiensUtilisateur = entretiensData.filter(
   (entretien) => entretien.managerId === userId || entretien.employeeId === userId
 )
+
+const entretiensEnAttente = ref([])
+const entretiensCompletes = ref([])
+
+// Séparer les entretiens en attente et complétés
+entretiensUtilisateur.forEach((entretien) => {
+  if (entretien.status === 'completed') {
+    entretiensCompletes.value.push(entretien)
+  } else {
+    entretiensEnAttente.value.push(entretien)
+  }
+})
 </script>
