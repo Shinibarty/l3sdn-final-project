2b994e0527fe207384990254274c53adc7dbbad2
Shinibarty <albanstievenard59@gmail.com>
2024-04-17 09:52:35 +0200

    ajout bouton détails sur entretiens

--- src/pages/InterviewsPage.vue
+++ src/pages/InterviewsPage.vue
@@ -2,79 +2,129 @@
   <div class="interviews-page">
     <q-page class="q-pa-md">
       <h2 class="text-h5">Mes entretiens</h2>
-      
+
       <!-- Affichage des onglets -->
-      <div v-if="entretiensEnAttenteInterviewer.length === 0 && entretiensCompletesInterviewer.length === 0 &&  entretiensEnAttenteInterviewee.length === 0 && entretiensCompletesInterviewee.length === 0"
-      class="text-center q-mt-lg">
-      </div>
+      <div
+        v-if="
+          entretiensEnAttenteInterviewer.length === 0 &&
+          entretiensCompletesInterviewer.length === 0 &&
+          entretiensEnAttenteInterviewee.length === 0 &&
+          entretiensCompletesInterviewee.length === 0
+        "
+        class="text-center q-mt-lg"
+      ></div>
       <div v-else>
         <q-tabs v-model="selectedTab">
-        <q-tab v-if="isManagerOrRH" name="interviewer">Entretiens réalisés</q-tab>
-        <q-tab name="interviewee">Entretiens en tant qu'interviewé</q-tab>        
-        </q-tabs> 
-      </div>      
-      
+          <q-tab v-if="isManagerOrRH" name="interviewer">Entretiens réalisés</q-tab>
+          <q-tab name="interviewee">Entretiens en tant qu'interviewé</q-tab>
+        </q-tabs>
+      </div>
+
       <!-- Contenu des onglets -->
       <q-tab-panels v-model="selectedTab">
         <!-- Entretiens en tant qu'interviewer -->
         <q-tab-panel name="interviewer">
           <!-- S'il n'y en a pas-->
-          <div v-if="entretiensEnAttenteInterviewer.length === 0 && entretiensCompletesInterviewer.length === 0" class="text-center q-mt-lg">
+          <div
+            v-if="
+              entretiensEnAttenteInterviewer.length === 0 &&
+              entretiensCompletesInterviewer.length === 0
+            "
+            class="text-center q-mt-lg"
+          >
             Aucun entretien trouvé.
           </div>
           <!-- S'il y en a -->
-            <div>
-            <!-- Entretiens en attente -->            
+          <div>
+            <!-- Entretiens en attente -->
             <h3 v-if="entretiensEnAttenteInterviewer.length > 0" class="text-h6">En attente</h3>
-            <q-card v-for="entretien in entretiensEnAttenteInterviewer" :key="entretien.id" class="q-mb-md bg-blue-grey-3">
+            <q-card
+              v-for="entretien in entretiensEnAttenteInterviewer"
+              :key="entretien.id"
+              class="q-mb-md bg-blue-grey-3"
+            >
               <q-card-section>
                 <div class="row">
                   <div class="col"><b>Date :</b> Le {{ entretien.date }}</div>
                   <div class="col"><b>Avec :</b> {{ getSecondPersonName(entretien) }}</div>
+                  <div v-if="isManagerOrRH" class="col">
+                    <q-btn color="primary" label="Détails" @click="openDetails(entretien)" />
+                  </div>
                 </div>
                 <div class="q-mt-sm"><b>Notes :</b> {{ entretien.notes }}</div>
               </q-card-section>
             </q-card>
             <!-- Entretiens complétés-->
-            <h3 v-if="entretiensCompletesInterviewer.length > 0" class="text-h6 q-mt-md">Complétés</h3>
-            <q-card v-for="entretien in entretiensCompletesInterviewer" :key="entretien.id" class="q-mb-md bg-light-green-5">
+            <h3 v-if="entretiensCompletesInterviewer.length > 0" class="text-h6 q-mt-md">
+              Complétés
+            </h3>
+            <q-card
+              v-for="entretien in entretiensCompletesInterviewer"
+              :key="entretien.id"
+              class="q-mb-md bg-light-green-5"
+            >
               <q-card-section>
                 <div class="row">
                   <div class="col"><b>Date :</b> Le {{ entretien.date }}</div>
                   <div class="col"><b>Avec :</b> {{ getSecondPersonName(entretien) }}</div>
+                  <div v-if="isManagerOrRH" class="col">
+                    <q-btn color="primary" label="Détails" @click="openDetails(entretien)" />
+                  </div>
                 </div>
                 <div class="q-mt-sm"><b>Notes :</b> {{ entretien.notes }}</div>
               </q-card-section>
             </q-card>
           </div>
         </q-tab-panel>
-        
+
         <!-- Entretiens en tant qu'interviewé -->
         <q-tab-panel name="interviewee">
           <!-- S'il n'y en a pas-->
-          <div v-if="entretiensEnAttenteInterviewee.length === 0 && entretiensCompletesInterviewee.length === 0" class="text-center q-mt-lg">
+          <div
+            v-if="
+              entretiensEnAttenteInterviewee.length === 0 &&
+              entretiensCompletesInterviewee.length === 0
+            "
+            class="text-center q-mt-lg"
+          >
             Aucun entretien trouvé.
           </div>
           <!-- S'il y en a -->
           <div>
             <!-- Entretiens en attente -->
             <h3 v-if="entretiensEnAttenteInterviewee.length > 0" class="text-h6">En attente</h3>
-            <q-card v-for="entretien in entretiensEnAttenteInterviewee" :key="entretien.id" class="q-mb-md bg-blue-grey-3">
+            <q-card
+              v-for="entretien in entretiensEnAttenteInterviewee"
+              :key="entretien.id"
+              class="q-mb-md bg-blue-grey-3"
+            >
               <q-card-section>
                 <div class="row">
                   <div class="col"><b>Date :</b> Le {{ entretien.date }}</div>
                   <div class="col"><b>Avec :</b> {{ getSecondPersonName(entretien) }}</div>
+                  <div v-if="isManagerOrRH" class="col">
+                    <q-btn color="primary" label="Détails" @click="openDetails(entretien)" />
+                  </div>
                 </div>
                 <div class="q-mt-sm"><b>Notes :</b> {{ entretien.notes }}</div>
               </q-card-section>
             </q-card>
             <!-- Entretiens complétés-->
-            <h3 v-if="entretiensCompletesInterviewee.length > 0" class="text-h6 q-mt-md">Complétés</h3>
-            <q-card v-for="entretien in entretiensCompletesInterviewee" :key="entretien.id" class="q-mb-md bg-light-green-5">
+            <h3 v-if="entretiensCompletesInterviewee.length > 0" class="text-h6 q-mt-md">
+              Complétés
+            </h3>
+            <q-card
+              v-for="entretien in entretiensCompletesInterviewee"
+              :key="entretien.id"
+              class="q-mb-md bg-light-green-5"
+            >
               <q-card-section>
                 <div class="row">
                   <div class="col"><b>Date :</b> Le {{ entretien.date }}</div>
                   <div class="col"><b>Avec :</b> {{ getSecondPersonName(entretien) }}</div>
+                  <div v-if="isManagerOrRH" class="col">
+                    <q-btn color="primary" label="Détails" @click="openDetails(entretien)" />
+                  </div>
                 </div>
                 <div class="q-mt-sm"><b>Notes :</b> {{ entretien.notes }}</div>
               </q-card-section>
@@ -99,7 +149,7 @@ const entretiensUtilisateur = entretiensData.filter(
   (entretien) => entretien.managerId === userId || entretien.employeeId === userId
 )
 
-// Séparer les entretiens où l'utilisateur est interviewer ou interviewé
+// Tri entretiens
 const entretiensEnAttenteInterviewer = ref([])
 const entretiensCompletesInterviewer = ref([])
 const entretiensEnAttenteInterviewee = ref([])
@@ -129,7 +179,7 @@ const getSecondPersonName = (entretien) => {
 }
 
 const isManagerOrRH = computed(() => {
-  const user = users.find(u => u.id === userId)
+  const user = users.find((u) => u.id === userId)
   return user && (user.role === 'manager' || user.role === 'RH')
 })
 
